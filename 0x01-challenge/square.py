4154c4e7201a981e6a7d533620f0aebcf1d2b8c1
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-14 13:17:21 +0200

    Fixing a Square module

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square():
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
