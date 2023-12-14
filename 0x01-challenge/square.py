8cfe7462452c71b22f563d3413872799149217ad
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-12-14 13:33:41 +0200

    Fixing a Square module

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,10 +3,12 @@
 
 
 class Square():
+    """Square class"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +17,15 @@ class Square():
         return self.width * self.width
 
     def permiter_of_my_square(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
